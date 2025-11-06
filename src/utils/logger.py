3f9dc1285ae2a266eacd080e613884a014718085
mcharniuk1-spec <mcharniuk1@gmail.com>
2025-11-06 11:03:21 +0200

    Add logger setup utility function

---
+++ src/utils/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+def setup_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    fmt = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    handler.setFormatter(fmt)
+    logger.addHandler(handler)
+    return logger
